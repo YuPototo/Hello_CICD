823929c0287cf006e68fa4fb05d7eda9ec80074f
Yu Qin <sirius.qinyu@gmail.com>
2022-06-28 12:23:10 +0800

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello CICD" });
+    res.json({ message: "Hello CICD!" });
 });
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
