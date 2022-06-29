21cbe04cd53913d97319b20106b0217ade379b4f
Yu Qin <sirius.qinyu@gmail.com>
2022-06-29 10:33:51 +0800

    use pm2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
     console.log("receive request");
-    res.json({ message: "Hello CICD! v101" });
+    res.json({ message: "Hello CICD! v102" });
 });
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
